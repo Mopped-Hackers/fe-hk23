bf32995116cb285dd61ebe755a25387c73cb96ed
williambrach <wibrach@gmail.com>
2023-04-02 06:50:08 +0200

    vote page

--- src/main/views/VoteView.js
+++ src/main/views/VoteView.js
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {css} from "@emotion/react";
+import React, { useEffect, useState } from 'react';
+import { css } from "@emotion/react";
 import config from "../../config";
-import {getData, routes} from "../routes";
-import Map, {Layer, Marker, Source} from "react-map-gl";
+import { getData, routes } from "../routes";
+import Map, { Layer, Marker, Source } from "react-map-gl";
 import * as turf from "@turf/turf";
 import Sidebar from "../components/Sidebar";
 import axios from "axios";
 import RenderCondition from "../../core/helpers/RenderCondition";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Arrow from "/src/assets/icons/arrow.svg"
 import Thumb from "/src/assets/icons/thumb.svg"
 
@@ -15,43 +15,60 @@ import Thumb from "/src/assets/icons/thumb.svg"
 
 export default function VoteView() {
 
-    const {map_css, sidebar_css} = styles();
-    const [markers,setMarkers] = useState(null);
-    const [data,setData] = useState(null)
+    const { map_css, sidebar_css } = styles();
+    const [markers, setMarkers] = useState(null);
+    const [data, setData] = useState(null)
 
+    const [voted, setVoted] = useState(false)
     const [currentPoint, setCurrentPoint] = useState(null);
 
-    useEffect(()=>{
+    useEffect(() => {
 
-        axios.get("http://vps.andrejvysny.sk:8000/geom/all?vote=1").then(r=>{
+        axios.get("http://vps.andrejvysny.sk:8000/geom/all?vote=1").then(r => {
 
 
             console.log(r.data);
             let points = [];
 
-            r.data.map((point,index)=>{
-                points.push(<Marker key={index} longitude={point.geometry.coordinates[0]} latitude={point.geometry.coordinates[1]} color={"orange"} onClick={()=>click(point)}><span style={{borderRadius: '50%',border: "2px solid black",opacity: 0.8, width:25,height:25,cursor:"pointer", backgroundColor:'red', display:"block"}}></span></Marker>)
+            r.data.map((point, index) => {
+                points.push(<Marker key={index} longitude={point.geometry.coordinates[0]} latitude={point.geometry.coordinates[1]} color={"orange"} onClick={() => click(point)}><span style={{ borderRadius: '50%', border: "2px solid black", opacity: 0.8, width: 25, height: 25, cursor: "pointer", backgroundColor: 'red', display: "block" }}></span></Marker>)
             });
 
             setMarkers(points);
 
-        }).catch(e=>console.log(e))
+        }).catch(e => console.log(e))
 
 
-    },[]);
+    }, []);
 
 
-    const click = point =>{
+    const click = point => {
         setCurrentPoint(point);
         console.log(point);
     }
 
 
+    const callOpinion = (plus) => {
+        console.log(currentPoint)
+        if (plus) {
+            axios.get(`http://vps.andrejvysny.sk:8000/geom/score/plus/${currentPoint?.properties.fid}`).then(r => {
+                console.log(r);
+            })
+
+        } else {
+            axios.get(`http://vps.andrejvysny.sk:8000/geom/score/minus/${currentPoint?.properties.fid}`).then(r => {
+                console.log(r);
+            })
+        }
+        setVoted(true)
+
+    }
+
 
 
     return (
         <>
-            <div  css={map_css}>
+            <div css={map_css}>
                 <Map
                     mapStyle={config.mapbox.style}
                     mapboxAccessToken={config.mapbox.access_token}
@@ -65,30 +82,48 @@ export default function VoteView() {
                 </Map>
             </div>
             <div className="vote_bar">
-            <div className="content">
+                <div className="content">
 
-            <div className='tab_switches_wrapper'>
-                <div className="tab_switches">
-                  <div className='back'><Link className="link" to={routes.home}><img src={Arrow}></img>Go back to default screen</Link></div> 
-                </div>
-            </div>
-                <div className='tab_switches_wrapper'> </div>
-                            <div className='in_fifteen_wrapper'>
-                            <RenderCondition condition={currentPoint !== null}>
-                                <h1 className='votingName'>{currentPoint?.properties.name}</h1>
+                    <div className='tab_switches_wrapper'>
+                        <div className="tab_switches">
+                            <div className='back'><Link className="link" to={routes.home}><img src={Arrow}></img>Go back to default screen</Link></div>
+                        </div>
+                    </div>
+                    <div className='tab_switches_wrapper'> </div>
+                    <div className='in_fifteen_wrapper'>
+                        <RenderCondition condition={currentPoint !== null}>
+                            <a target="_blank"
+                                href={"https://www.google.com/maps/place/" + currentPoint?.geometry.coordinates[1] + ',' + currentPoint?.geometry.coordinates[0]}
+                            >
+                                <i className="fab pe-2 fa-google" />
+                                <h1 className='votingName'>
+                                    {currentPoint?.properties.name}
+                                </h1>
                                 <p className='votingAdress'>{currentPoint?.properties.addressline}</p>
-                                <p className="votingCoordinates">{currentPoint?.geometry.coordinates.toString()}</p>
-                                <p className="votingInfo"> {currentPoint?.properties.info}</p>
+                            </a>
 
+                            <p className="votingCoordinates">Poloha - {currentPoint?.geometry.coordinates.toString()}</p>
+                            <p className="votingInfo"> {currentPoint?.properties.info}</p>
+
+                            <RenderCondition condition={!voted}>
                                 <p className='opinion'>Express your opinion</p>
                                 <div className="voting_buttons">
-                                    <div className="yes center btn"><img src={Thumb}></img>I agree</div>
-                                    <div className="no center btn"><img className="thumbRotate"src={Thumb}></img>I disagree</div>
+                                    <div className="yes center btn" onClick={() => callOpinion(true)}><img src={Thumb}></img>I agree</div>
+                                    <div className="no center btn" onClick={() => callOpinion(false)} ><img className="thumbRotate" src={Thumb}></img>I disagree</div>
                                 </div>
                             </RenderCondition>
-                            </div>
+                            <RenderCondition condition={voted}>
+                                <p className='opinion'>Thank you for your honest opinion!</p>
+                            </RenderCondition>
+
+
+                        </RenderCondition>
+                        <RenderCondition condition={currentPoint == null}>
+                            <p className='opinion'>Please select point for voting.</p>
+                        </RenderCondition>
                     </div>
-                
+                </div>
+
             </div>
         </>
     );
